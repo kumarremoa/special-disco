f0ac28038961fb32c289eb6c281058bbcc057155
Zachary Skiles <skilesz@purdue.edu>
2018-10-13 16:53:41 -0400

    start website beaver tutorial

---
+++ source/WebRTC.js
@@ -0,0 +1,34 @@
+MediaStream {
+    active:true,
+    id:"ARwKgYl2LvuZyw2zWzKWeGzXUEy0KHpJj9xW",
+    onactive:null,
+    onaddtrack:null,
+    oninactive:null,
+    onremovetrack:null
+   }
+
+   RTCPeerConnection {
+    iceConnectionState:"new",
+    iceGatheringState:"new",
+    localDescription:RTCSessionDescription {
+    sdp:"",
+    type:""
+    },
+    onaddstream:null,
+    ondatachannel:null,
+    onicecandidate:null,
+    oniceconnectionstatechange:null,
+    onnegotiationneeded:null,
+    onremovestream:null,
+    onsignalingstatechange:null,
+    remoteDescription:RTCSessionDescription {
+    sdp:"",
+    type:""
+    },
+    signalingState:"stable"
+   }
+
+   {
+    type: "offer", 
+    sdp: "v=1↵c=- 5245133456194626701 2 IN IP4 127.0.0.1↵s…3610 label:br19t341-rd8t-94tb-rE8j-p4625gt469y5↵"
+   }
